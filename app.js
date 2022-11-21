50bc75fc4589aff08d9d275fe8619d6cf9e0b89e
Eric Miller <eric.miller4@nih.gov>
2022-11-21 16:45:01 -0500

    formatting, route and port updates

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const fs = require('fs');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const fs = require("fs");
+const cors = require("cors");
 
-const LOG_FOLDER = 'logs';
+const LOG_FOLDER = "logs";
 if (!fs.existsSync(LOG_FOLDER)) {
   fs.mkdirSync(LOG_FOLDER);
 }
 
 // create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(path.join(__dirname, LOG_FOLDER, 'access.log'), { flags: 'a'})
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, LOG_FOLDER, "access.log"),
+  { flags: "a" }
+);
 
-const devOpsRouter = require('./routes/devOps');
+const interoperationsRouter = require("./routes/interoperation");
 
 const app = express();
 app.use(cors());
 
 // setup the logger
-app.use(logger('combined', { stream: accessLogStream }))
+app.use(logger("combined", { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/devOps', devOpsRouter);
+app.use("/api/interoperation", interoperationsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next({status: 404, message: `Path: '${req.path}' is not supported!`});
+app.use(function (req, res, next) {
+  next({ status: 404, message: `Path: '${req.path}' is not supported!` });
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  const message = req.app.get('env') === 'development' ? err.message : 'error';
+app.use(function (err, req, res, next) {
+  const message = req.app.get("env") === "development" ? err.message : "error";
 
   // render the error page
   res.status(err.status || 500);
