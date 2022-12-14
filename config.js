0a38e9c694b2f08b133b39b3f729f3a1ddebe299
Eric Miller <eric.miller4@nih.gov>
2022-12-14 16:38:23 -0500

    read/load ENV variable for bento backend API URI

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ dotenv.config();
 const config = {
   version: process.env.VERSION,
   date: process.env.DATE,
+  BENTO_BACKEND_GRAPHQL_URI: process.env.BENTO_BACKEND_GRAPHQL_URI,
 };
 
 if (!config.version) {
