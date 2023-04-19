cc8c2d72b29bd476e6bf4876abdfc7dc50a6b6d1
Eric Miller <eric.miller4@nih.gov>
2023-04-19 16:04:54 -0400

    debugging redis unavailable error

--- redis/redis-client.js
+++ redis/redis-client.js
@@ -4,11 +4,21 @@ const config = require("../config");
 let redisClient;
 
 (async () => {
-  redisClient = redis.createClient(config.REDIS_HOST, config.REDIS_PORT);
-  redisClient.on("error", (error) =>
-    console.error(`Redis client error: ${error}`)
-  );
+  redisClient = redis.createClient(config.REDIS_HOST, config.REDIS_PORT, {
+    disableOfflineQueue: true,
+  });
+  redisClient.on("error", async (error) => await redisClient.quit());
   await redisClient.connect();
 })();
+
+// async function connectToRedis() {
+//   let redisClient = redis.createClient(config.REDIS_HOST, config.REDIS_PORT, {
+//     socket: {
+//       reconnectStrategy: false,
+//     },
+//   });
+//   redisClient.on("error", (error) => console.error());
+//   await redisClient.connect();
+// }
 
 module.exports = redisClient;
