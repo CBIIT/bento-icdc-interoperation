ed621ab11e2da65a41943a41b6f334b84e184440
Eric Miller <eric.miller4@nih.gov>
2023-05-23 15:05:17 -0400

    remove unused error types

--- constants/error-constants.js
+++ constants/error-constants.js
@@ -1,8 +1,6 @@
 exports.errorName = {
   STUDY_CODE_NOT_FOUND: "STUDY_CODE_NOT_FOUND",
   BENTO_BACKEND_NOT_CONNECTED: "BENTO_BACKEND_NOT_CONNECTED",
-  MANIFEST_FILE_WRITE_ERROR: "MANIFEST_FILE_WRITE_ERROR",
-  MANIFEST_FILE_READ_ERROR: "MANIFEST_FILE_READ_ERROR",
 };
 
 exports.errorType = {
@@ -14,15 +12,5 @@ exports.errorType = {
   BENTO_BACKEND_NOT_CONNECTED: {
     message: "The server was unable to connect to a Bento backend instance.",
     statusCode: 503,
-  },
-  MANIFEST_FILE_WRITE_ERROR: {
-    message:
-      "The server encountered an error when writing manifest data to a file.",
-    statusCode: 500,
-  },
-  MANIFEST_FILE_READ_ERROR: {
-    message:
-      "The server encountered an error while reading manifest file data.",
-    statusCode: 500,
   },
 };
