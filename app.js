95ddf98251d77df7bb9bb809f858f9ba021af394
Eric Miller <eric.miller4@nih.gov>
2023-06-27 11:38:53 -0400

    reintroduce CORS config update

--- app.js
+++ app.js
@@ -18,16 +18,17 @@ const accessLogStream = fs.createWriteStream(
 );
 
 const app = express();
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://caninecommons-dev.cancer.gov",
-      "https://caninecommons-qa.cancer.gov",
-      "https://caninecommons.cancer.gov",
-    ],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://caninecommons-dev.cancer.gov",
+//       "https://caninecommons-qa.cancer.gov",
+//       "https://caninecommons.cancer.gov",
+//     ],
+//   })
+// );
+app.use(cors());
 
 // setup the logger
 app.use(logger("combined", { stream: accessLogStream }));
